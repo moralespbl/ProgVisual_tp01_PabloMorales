9aa4f83e75ad4398cac906dd8401e76c8703fda3
Pablo <Pablo@DESKTOP-L7A7O98>
2024-04-08 19:13:10 -0300

    Se agrega paquete y clase para el ejercicio 1

---
+++ src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,11 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// desarrollo del ejercicio 1
+			System.out.println("Ejercicio 1");
+	}
+
+}
+// https://github.com/moralespbl/ProgVisual_tp01_PabloMorales.git
